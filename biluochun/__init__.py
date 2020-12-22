b1e0da67e0f14a8fa3cb1a7d89b8431f8e916f7b
3TUSK <urey.s.knowledge@gmail.com>
2020-12-22 10:50:27 -0800

    Throw SECRET_KEY into config files instead of being hardcoded

--- biluochun/__init__.py
+++ biluochun/__init__.py
@@ -6,18 +6,12 @@ from .dashboard import init_dashboard
 from .model import init_db
 from flask import Flask
 from flask_cors import CORS
-import hashlib
-import secrets
-
-# Random secret key, used for preventing CSRF and others
-secret = hashlib.sha256(b"\x00" + secrets.token_bytes(30) + b"\x00").hexdigest()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_envvar('BILUOCHUN_CONFIG_PATH')
     # Wrapped with CORS
     CORS(app)
-    app.secret_key = secret
 
     init_db(app)
     init_authz(app)
