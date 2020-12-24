f998c7d3a2a6a2162195eabe1153141131ecde6c
3TUSK <urey.s.knowledge@gmail.com>
2020-12-24 12:07:55 -0800

    Expose team icon retrival API

--- biluochun/api.py
+++ biluochun/api.py
@@ -1,9 +1,13 @@
 from .model import Team, User, db
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, send_file, url_for
 from flask.json import jsonify
+from io import BytesIO
 
-def summary(team):
-    return { 'team': team.name, 'repo': team.repo }
+def summary(team, detailed = False):
+    info = { 'name': team.name, 'repo': team.repo }
+    if detailed:
+        info['desc'] = team.description
+    return info
 
 def init_api(app):
     bp = Blueprint('api', __name__, url_prefix = '/api')
@@ -19,5 +23,16 @@ def init_api(app):
             return { 'error': 'No such team' }, 404
         else:
             return summary(team)
+
+    @bp.route('/team/<team_name>/avatar')
+    @bp.route('/team/<team_name>/icon')
+    @bp.route('/team/<team_name>/profile_pic')
+    def get_team_icon():
+        team = Team.query.filter_by(name = team_name).first()
+        if team == None:
+            return { 'error': 'No such team' }, 404
+        else:
+            return send_file(BytesIO(team.profile_pic), mimetype = 'image/png')
+    
 
     app.register_blueprint(bp)
