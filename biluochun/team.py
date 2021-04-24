86cf727a56b5f8d7631ac1ce4d52fa7c97daf1df
3TUSK <urey.s.knowledge@gmail.com>
2021-04-24 13:42:00 -0700

    Refactor team.py
    - Add doc strings
    - Add converter to route variables
    - Reorganize team creation logic

--- biluochun/team.py
+++ biluochun/team.py
@@ -1,3 +1,7 @@
+'''
+Defines /api/team endpoints series.
+'''
+
 from io import BytesIO
 import secrets
 
@@ -11,6 +15,9 @@ from .model import Team, User, db
 from .util import cleanse_profile_pic, team_summary, user_summary
 
 def init_team_api(app):
+    '''
+    Initialize the app with /api/team endpoints series.
+    '''
     bp = Blueprint('api', __name__, url_prefix = '/api/team')
     
     @bp.route('/', methods = [ 'GET' ])
@@ -20,76 +27,69 @@ def init_team_api(app):
     @bp.route('/', methods = [ 'POST' ])
     @login_required
     def create_team():
-        if current_user.team_id is None or current_user.team_id <= 0:
+        if current_user.team_id is None:
             next_id = db.session.query(func.max(Team.id)).first()[0]
             if next_id:
                 next_id += 1
             else:
                 next_id = 1
-            team_name = f"{current_user.name}'s team"
-            mod_name = f"{current_user.name}'s mod"
-            team_desc = ""
-            repo_url = ""
+            new_team = Team(id = next_id, name = f"{current_user.name}'s team", \
+                mod_name = f"{current_user.name}'s mod", invite = secrets.token_hex(8))
+            current_user.team_id = new_team.id
             form = TeamInfo()
             if form.validate_on_submit():
-                if form.name is not None:
-                    team_name = form.name
-                if form.mod_name is not None:
-                    mod_name = form.mod_name
-                if form.desc is not None:
-                    team_desc = form.desc
-                if form.repo is not None:
-                    repo_url = form.repo
-            new_team = Team(id = next_id, name = team_name, invite = secrets.token_hex(8), \
-                mod_name = mod_name, description = team_desc, repo = repo_url)
+                if form.name.data is not None:
+                    new_team.name = form.name.data
+                if form.mod_name.data is not None:
+                    new_team.mod_name = form.mod_name.data
+                if form.desc.data is not None:
+                    new_team.description = form.desc.data
+                if form.repo.data is not None:
+                    new_team.repo = form.repo.data
             db.session.add(new_team)
-            current_user.team_id = new_team.id
             db.session.commit()
             return {}
         else:
             return { 'error': 'You have already been in a team!' }, 400
 
-    @bp.route('/<team_id>', methods = [ 'GET' ])
+    @bp.route('/<int:team_id>', methods = [ 'GET' ])
     def show_team(team_id):
         team = Team.query.get(team_id)
         return { 'error': 'No such team' }, 404 if team is None else team_summary(team, detailed = True)
 
-    @bp.route('/<team_id>', methods = [ 'POST' ])
+    @bp.route('/<int:team_id>', methods = [ 'POST' ])
     @login_required
     def update_team(team_id):
         team = Team.query.get(team_id)
         if team is None:
             return { 'error': 'No such team' }, 404
-
         if current_user.team_id != team.id:
             return { 'error': f"You are not in team '{team.name}'!" }, 400
-
         form = TeamInfo()
         if form.validate_on_submit():
-            try:
+            if form.name.data is not None:
                 team.name = form.name.data
+            if form.mod_name.data is not None:
                 team.mod_name = form.mod_name.data
+            if form.desc.data is not None:
                 team.description = form.desc.data
+            if form.repo.data is not None:
                 team.repo = form.repo.data
-                db.session.commit()
-                return {}
-            except Exception as e:
-                return { 'error': 'Error occured while updating info.', 'details': str(e) }, 500
+            db.session.commit()
+            return {}
         else:
             return {
                 'error': 'Form contains error. Check "details" field for more information.',
                 'details': form.errors
             }, 400
 
-    @bp.route('/<team_id>/members', methods = [ 'GET' ])
+    @bp.route('/<int:team_id>/members', methods = [ 'GET' ])
     def get_team_members(team_id):
         team = Team.query.get(team_id)
-        if team is None:
-            return { 'error': 'No such team' }, 404
-        else:
-            return { 'members': [ user_summary(member) for member in team.members ] }
+        return { 'error': 'No such team' }, 404 if team is None \
+            else jsonify([ user_summary(member) for member in team.members ])
     
-    @bp.route('/<team_id>/members', methods = [ 'POST', 'PATCH' ])
+    @bp.route('/<int:team_id>/members', methods = [ 'POST', 'PATCH' ])
     @login_required
     def add_team_members(team_id):
         team = Team.query.get(team_id)
@@ -104,35 +104,30 @@ def init_team_api(app):
                 db.session.commit()
             return { 'info': 'Not Yet Implemented' } 
 
-    @bp.route('/<team_id>/avatar', methods = [ 'GET' ])
-    @bp.route('/<team_id>/icon', methods = [ 'GET' ])
-    @bp.route('/<team_id>/profile_pic', methods = [ 'GET' ])
+    @bp.route('/<int:team_id>/avatar', methods = [ 'GET' ])
+    @bp.route('/<int:team_id>/icon', methods = [ 'GET' ])
+    @bp.route('/<int:team_id>/profile_pic', methods = [ 'GET' ])
     def get_team_icon(team_id):
         team = Team.query.get(team_id)
-        if team is None:
-            return { 'error': 'No such team' }, 404
-        else:
-            return send_file(BytesIO(team.profile_pic), mimetype = 'image/png')
+        return { 'error': 'No such team' }, 404 if team is None \
+            else send_file(BytesIO(team.profile_pic), mimetype = 'image/png')
     
-    @bp.route('/<team_id>/avatar', methods = [ 'POST' ])
-    @bp.route('/<team_id>/icon', methods = [ 'POST' ])
-    @bp.route('/<team_id>/profile_pic', methods = [ 'POST' ])
+    @bp.route('/<int:team_id>/avatar', methods = [ 'POST' ])
+    @bp.route('/<int:team_id>/icon', methods = [ 'POST' ])
+    @bp.route('/<int:team_id>/profile_pic', methods = [ 'POST' ])
     @login_required
     def update_team_icon(team_id):
         team = Team.query.get(team_id)
         if team is None:
             return { 'error': 'No such team' }, 404
-
         if current_user.team_id != team.id:
             return { 'error': f"You are not in team '{team.name}'!" }, 400
-        
         raw_img = None
         if form:
             form = Avatar()
             raw_img = form.avatar.data
         else:
             raw_img = request.stream # TODO Validate it
-            
         if raw_img:
             team.profile_pic = cleanse_profile_pic(raw_img)
             db.session.commit()
