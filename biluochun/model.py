efc9c5b1127b1e0570cfc855638b1e8c72a416c4
3TUSK <urey.s.knowledge@gmail.com>
2021-04-24 18:33:55 -0700

    Limit team name, mod name and user name length
    All of three column now have length limits capped at 128.
    Yes, long mod name happend once in TeaCon2020 (aka Kaleido).

--- biluochun/model.py
+++ biluochun/model.py
@@ -1,3 +1,7 @@
+'''
+Defines data model used by Biluochun.
+'''
+
 from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
 import flask_login
 from flask_sqlalchemy import SQLAlchemy
@@ -10,9 +14,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Team(db.Model):
+    '''
+    A team describes a base unit of participation.
+    '''
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.UnicodeText, nullable = False, unique = True)
-    mod_name = db.Column(db.UnicodeText, nullable = False, unique = True)
+    name = db.Column(db.Unicode(128), nullable = False, unique = True)
+    mod_name = db.Column(db.Unicode(128), nullable = False, unique = True)
     invite = db.Column(db.String(16), nullable = False, unique = True)
     profile_pic = db.Column(db.LargeBinary)
     description = db.Column(db.UnicodeText)
@@ -23,9 +30,12 @@ class Team(db.Model):
         return f"<Team #{self.id} '{self.name}' repo={self.repo}>"
 
 class User(db.Model, flask_login.UserMixin):
+    '''
+    A user describes a participtant of the event.
+    '''
     id = db.Column(db.Integer, primary_key = True)
     ms_id = db.Column(db.String(16), nullable = False, unique = True)
-    name = db.Column(db.UnicodeText, nullable = False, unique = True)
+    name = db.Column(db.Unicode(128), nullable = False, unique = True)
     profile_pic = db.Column(db.LargeBinary)
     team_id = db.Column(db.Integer, db.ForeignKey("team.id"))
     team = db.relationship(Team)
@@ -37,10 +47,16 @@ class User(db.Model, flask_login.UserMixin):
         return f"<User #{self.id} '{self.name}'"
 
 class OAuth(db.Model, OAuthConsumerMixin):
+    '''
+    An OAuth object represents the OAuth data of the user.
+    '''
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     user = db.relationship(User)
 
 def init_db(app):
+    '''
+    Initialize the database. Tables are created if not existed yet.
+    '''
     db.init_app(app)
     with app.app_context():
         db.create_all()
