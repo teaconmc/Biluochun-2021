3400fd40076338277bda92f3b23becd26edf7e10
3TUSK <urey.s.knowledge@gmail.com>
2021-04-30 11:16:33 -0700

    404

--- biluochun/image.py
+++ biluochun/image.py
@@ -16,10 +16,8 @@ def init_image(app):
 
     @bp.route('/<int:img_id>')
     def get_image(img_id):
-        if img_id == 1:
-            return redirect(app.config['BILUOCHUN_DEFAULT_USER_AVATAR'])
-        if img_id == 2:
-            return redirect(app.config['BILUOCHUN_DEFAULT_TEAM_AVATAR'])
+        if img_id == 1 or img_id == 2:
+            return { 'error': 'Not found' }, 404
         img = Image.query.get(img_id)
         return { "error": "Not found" }, 404 if img is None \
             else send_file(BytesIO(img.data), mimetype = 'image/png')
