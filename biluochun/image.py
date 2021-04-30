fb3b4abb5dd8dc1025700804724154ee3dd60e06
3TUSK <urey.s.knowledge@gmail.com>
2021-04-30 11:37:20 -0700

    No tenery if-else expression

--- biluochun/image.py
+++ biluochun/image.py
@@ -16,10 +16,9 @@ def init_image(app):
 
     @bp.route('/<int:img_id>')
     def get_image(img_id):
-        if img_id == 1 or img_id == 2:
-            return { 'error': 'Not found' }, 404
         img = Image.query.get(img_id)
-        return { "error": "Not found" }, 404 if img is None \
-            else send_file(BytesIO(img.data), mimetype = 'image/png')
+        if img is None:
+            return { "error": "Not found" }, 404
+        return send_file(BytesIO(img.data), mimetype = 'image/png')
 
     app.register_blueprint(bp)
